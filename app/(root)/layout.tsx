9190179dd00ac9205a062fced2b4b5259e3058aa
Beka-Merebashvili <beqamerebashvili26@gmail.com>
2025-07-08 13:55:39 +0400

    finish sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,23 +1,29 @@
-import Header from '@/components/Header';
-import MobileNavigation from '@/components/MobileNavigation';
-import Sidebar from '@/components/Sidebar';
-import React from 'react'
+import React from "react";
+import Sidebar from "@/components/Sidebar";
+import MobileNavigation from "@/components/MobileNavigation";
+import Header from "@/components/Header";
+import { getCurrentUser } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
+// import { Toaster } from "@/components/ui/toaster";
 
-const Layout = ({ children }: { children: React.ReactNode }) => {
+// export const dynamic = "force-dynamic";
 
+const Layout = async ({ children }: { children: React.ReactNode }) => {
+  const currentUser = await getCurrentUser();
 
+  if (!currentUser) return redirect("/sign-in");
 
   return (
     <main className="flex h-screen">
-      <Sidebar />
+      <Sidebar {...currentUser} />
 
       <section className="flex h-full flex-1 flex-col">
-        <MobileNavigation />
-        <Header />
+        <MobileNavigation {...currentUser} />
+        <Header /> 
         <div className="main-content">{children}</div>
       </section>
 
-    
+      {/* <Toaster /> */}
     </main>
   );
 };
