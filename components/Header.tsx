03d015c893eb81dd99c883df6a43439afb0786bf
Beka-Merebashvili <beqamerebashvili26@gmail.com>
2025-07-12 15:17:55 +0400

    add file upload function

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,17 +1,23 @@
-import React from 'react'
+import React from "react";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import Search from './Search';
-import FileUploader from './FileUploader';
-import { signOutUser } from '@/lib/actions/user.actions';
+import Search from "@/components/Search";
+import FileUploader from "@/components/FileUploader";
+import { signOutUser } from "@/lib/actions/user.actions";
 
-const Header = () => {
+const Header = ({
+  userId,
+  accountId,
+}: {
+  userId: string;
+  accountId: string;
+}) => {
   return (
     <header className="header">
       <Search />
       <div className="header-wrapper">
-       <FileUploader /> 
-         <form
+        <FileUploader ownerId={userId} accountId={accountId} />
+        <form
           action={async () => {
             "use server";
 
@@ -30,7 +36,6 @@ const Header = () => {
         </form>
       </div>
     </header>
-  )
-}
-
-export default Header
+  );
+};
+export default Header;
